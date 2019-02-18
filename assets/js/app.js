fd0642c80c78c047a6a44eef9ad97c459efe05d2
dager <zndager@gmail.com>
2019-02-17 22:32:54 -0600

    Avance 1
    menu vertical y horizontal listo. Secciones en modo slide terminado.

---
+++ assets/js/app.js
@@ -0,0 +1,44 @@
+(function($) {      
+
+    $("#button-hamburger").click(function() {
+        $("#button-hamburger").toggleClass('group')
+        $('.menu-horizontal__list').toggleClass('menu-horizontal__list--open')
+    });
+
+    $('#list-horizontal').click(function(event){
+        let prevId = $('.subsection-active').attr('id'),
+            target = $(event.target),
+            idToGo = (target).attr('id')
+
+            changePage(idToGo, prevId)
+    })
+
+    $('#list').click(function(event){
+
+        let prevId = $('.subsection-active').attr('id'),
+            target = $(event.target),
+            idToGo = (target).attr('id')
+
+        if(target.is("span")){
+            idToGo = (target).parent().attr('id')
+        }
+
+        changePage(idToGo, prevId)
+    })
+  
+    function changePage(idToGo, prevId){
+        $('#'+prevId).siblings().removeClass('subsection-prev')
+        $('#'+prevId).addClass('subsection-prev')
+        $('#'+idToGo).siblings().removeClass('list_item-active')
+        $('#'+idToGo).addClass('list_item-active')
+        indexOfUnderscore = idToGo.indexOf('-')
+        idToGo = idToGo.substring(0, indexOfUnderscore)
+        idToGo = idToGo+'-section'
+        $('#'+idToGo).siblings().removeClass('subsection-active')
+        if(!$('#'+idToGo).hasClass('subsection-active'))
+            $('#'+idToGo).addClass('subsection-active')
+    }
+
+
+    
+})(jQuery);
---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const gulp = require('gulp'),
+      browserSync = require('browser-sync').create(),
+      sass = require('gulp-sass'),
+      autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('default', () =>{
+    browserSync.init({
+        server: './'
+    })
+    gulp.watch('./assets/sass/*.scss', gulp.series('sass'));
+    gulp.watch(['./*.html', './assets/css/style.css', './assets/js/*.js']).on('change', browserSync.reload)
+
+});
+
+gulp.task('sass', ()=>
+    gulp.src('./assets/sass/style.scss')
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+
+        .pipe(gulp.dest('./assets/css/'))
+);
