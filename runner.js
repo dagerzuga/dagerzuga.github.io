e6952fb6472ad50f88e893d63d64ec8cf20ddcc5
Dáger Zúñiga N <dagerzuga@gmail.com>
2021-05-15 17:38:32 -0600

    Refacoting js

--- runner.js
+++ runner.js
@@ -1,24 +1,35 @@
-document.addEventListener('click', function(event) {
-  if (!event.target.matches('.btn-scroll-into')) return;
-  event.preventDefault();
-  var element = document.getElementById(event.target.dataset.target);
-  element.scrollIntoView({behavior: 'smooth'});
-});
+(function () {
+  function initScrollMenu() {
+    document.addEventListener('click', event => {
+      if (!event.target.matches('.js-btn-scroll-into')) return;
+      event.preventDefault();
+      let element = document.getElementById(event.target.dataset.target);
+      element.scrollIntoView({behavior: 'smooth'});
+    });
+  }
 
+  function showPosts() {
+    const endpoint = 'https://weeklydev.blog//wp-json/wp/v2/posts?per_page=9';
+    fetch(endpoint)
+        .then(response => response.json())
+        .then(json => filterPosts(json));
+  }
 
+  function filterPosts(elements) {
+    elements.forEach(createPostCard);
+    hideArticlePlaceHolder();
+  }
 
+  function createPostCard(item, index) {
+    let articleIndex = `article-${index}`;
+    document.getElementById(articleIndex).innerHTML = item.title.rendered;
+    document.getElementById(articleIndex).href = item.link
+  }
 
-function filterPosts(elements) {
-  elements.forEach(createPostCard);
-}
+  function hideArticlePlaceHolder() {
+    document.getElementById('articles-place-holder').classList.add('d-none');
+  }
 
-function createPostCard(item, index) {
-  var articleIndex = 'article-'+index;
-  document.getElementById(articleIndex).innerHTML = item.title.rendered;
-  document.getElementById(articleIndex).href = item.link
-}
-
-const endpoint = 'https://weeklydev.blog//wp-json/wp/v2/posts?per_page=9';
-fetch(endpoint)
-    .then(response => response.json())
-    .then(json => filterPosts(json));
+  initScrollMenu();
+  showPosts();
+})();
